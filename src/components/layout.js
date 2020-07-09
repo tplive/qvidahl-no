d5d7c5ef22f8a9b8657a2a73c4be7ececdb810bd
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-07-09 15:29:33 +0200

    Move footer from layout to own component. Add about page. Add blog page. Begin moving header to own component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,7 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header title={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
@@ -34,11 +35,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer/>
       </div>
     </>
   )
