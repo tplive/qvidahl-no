8b4cac1d3c76eb11ccd90fdd88b720f52ba2813d
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-07-10 08:18:08 +0200

    Remove comment. Change prop name.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -26,7 +19,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header title={data.site.siteMetadata.title} />
+      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
