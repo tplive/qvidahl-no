1c4c88c4fddad7a27aaa7368b79f4c64e6c25359
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2020-12-05 20:54:48 +0100

    Adds links to Skomakergata videos

--- src/components/adventskalender.js
+++ src/components/adventskalender.js
@@ -3,7 +3,23 @@ import Card from "../components/card"
 import julestyle from "./adventskalender.module.css"
 
 console.log(julestyle);
-// Episodes 1-24 on Youtube
+
+var skomakergata = [
+"https://youtu.be/XvB-cf2g2Zw", "https://youtu.be/5k21zKOJNCU", 
+"https://youtu.be/K1dkuYYe7xk", "https://youtu.be/xinpW9k26qo", 
+"https://youtu.be/vEW6JaV9ky0", "https://youtu.be/jyNvAanHt1M", 
+"https://youtu.be/FABLteuq7Y4", "https://youtu.be/b1-pToNRh60", 
+"https://youtu.be/Z8tcr6kFAZQ", "https://youtu.be/iNP7Yiuba0k", 
+"https://youtu.be/Oyph01G_yGs", "https://youtu.be/IFZNCsVDGo4", 
+"https://youtu.be/elwVaH_IBbc", "https://youtu.be/LO5Y4wiCu-Y", 
+"https://youtu.be/7EKIhCk5u_0", "https://youtu.be/6NnKk5p_HOg", 
+"https://youtu.be/16ULOYIRxyw", "https://youtu.be/m8GTaKlat-I", 
+"https://youtu.be/5nE0Zn2Et94", "https://youtu.be/1opSFU3C27I", 
+"https://youtu.be/mWZ80uuAmgs", "https://youtu.be/Y-l_0POyEBg", 
+"https://youtu.be/HgraVecKNRk", "https://youtu.be/-wK27zOA5gA", 
+];
+
+// Episodes 1-24 of The Julekalender on Youtube
 var episodes = ["https://youtu.be/bFHy61erVyQ", "https://youtu.be/OaUoWeBbYAo",
     "https://youtu.be/qBegaNpUp7o", "https://youtu.be/UVPOXFnDjvM",
     "https://youtu.be/x8mrLF0c51g", "https://youtu.be/rVZoL6ZvJwk",
