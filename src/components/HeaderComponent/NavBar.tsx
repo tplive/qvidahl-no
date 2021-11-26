50658a0dc77c266eeb4e1fed2042114f9ea65f7f
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-26 21:06:14 +0100

    Try add interface

--- src/components/HeaderComponent/NavBar.tsx
+++ src/components/HeaderComponent/NavBar.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class NavBar {
+interface NavBar { }
+
+class NavBar extends Component {
   render() {
     return (
       <header>
