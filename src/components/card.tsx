775eef48b0ed8996fc08ff9c7ac414825384fcef
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-26 19:37:36 +0100

    Experiment. Import previously used components.

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import julestyle from './card.module.css';
 
 const Card = ({ number, url, active }: { number: number, url: string, active: any }) => {
