8bdb8ae57b05171d75577f19b381c0dbe1f72933
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-26 21:06:36 +0100

    Simplify

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-//import { browserHistory } from 'react-router';
-import HomePage from './HomePage';
 import NavBar from './HeaderComponent/NavBar';
 
 class App extends Component {
