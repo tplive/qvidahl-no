a3ed33e551624e1fb200d4f1694a2d25d37b6fbd
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-28 17:58:21 +0100

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './components/App'
+import './styles/global.css'
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
