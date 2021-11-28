a038e2b96e8581d1e038e7a1fd52686a96e09083
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-28 17:55:35 +0100

    Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,106 @@
-const webpack = require('webpack');
-const path = require('path');
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const config = {
-    entry: [
-        'react-hot-loader/patch',
-        './src/index.tsx'
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /(node_modules)/,
+const webpack = require('webpack')
+const middleware = require('webpack-dev-middleware')
+const path = require('path')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+
+    const config = {
+        entry: ['react-hot-loader/patch', './src/index.tsx'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                    exclude: /\.module\.css$/,
+                },
+                {
+                    test: /\.ts(x)?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.scss$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: isProduction || {
+                                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                                },
+                            },
+                        },
+                    ],
+                    include: /\.module.css$/,
+                },
+                {
+                    test: /\.svg$/,
+                    use: 'file-loader',
+                },
+                {
+                    test: /\.png$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                mimetype: 'image/png',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.tsx', '.ts'],
+            alias: {
+                'react-dom': '@hot-loader/react-dom',
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.png$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            mimetype: 'image/png'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.ts(x)?$/,
-                exclude: /(node_modules)/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    },
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 8090,
-    },
-    resolve: {
-        extensions: [
-            '.tsx',
-            '.ts',
-            '.js'
+        },
+        devServer: {
+            static: true,
+        },
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                openAnalyzer: false,
+            }),
+            new MiniCssExtractPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
         ],
-        alias: {
-            'react-dom': '@hot-loader/react-dom'
-        }
+        mode: isProduction ? 'production' : 'development',
     }
-};
 
-module.exports = config;
+    if (!isProduction) {
+        const compiler = webpack(config)
+
+        middleware(compiler, {
+            writeToDisk: true,
+        })
+    }
+
+    return config
+}
