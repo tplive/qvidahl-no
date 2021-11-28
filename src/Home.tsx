fcc8f4206bd3f00c7f0c443f1d823a10e75715be
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-11-28 17:57:33 +0100

    Rename and update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import * as React from 'react'
 
 function Home() {
   return (
     <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              className="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Home</h1>
             <p>
               This is a short paragraph.
             </p>
