72cfab8421c3d3dc6d8acc0b4b3cef059127e207
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-12-02 22:25:58 +0100

    Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
-//import TopMenu from '../components/TopMenu'
+import TopMenu from '../components/TopMenu'
 //import ContentCards from '../components/ContentCards'
 //import Quotes from '../components/Quotes'
 //import ImageCarousel from '../components/ImageCarousel'
@@ -11,21 +11,24 @@ import Header from '../components/Header'
 import 'tailwindcss/tailwind.css'
 
 const Home: NextPage = () => {
+
+  const title = "Styrkr"
   return (
     <>
-      <div className="bg-green-600">
-        <Head>
-          <title>Styrkr</title>
-          <meta name="description" content="Styrkr is an old norse word for 'strength'" />
-          <link rel="icon" href="/assets/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
-        </Head>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="Styrkr is an old norse word for 'strength'" />
+        <link rel="icon" href="/assets/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=UnifrakturCook:wght@700&display=swap" rel="stylesheet" />
+      </Head>
 
-        <body>
-          <Header title="Styrkr" subtitle="This is subtitle" />
-        </body>
-
-      </div>
+      <body>
+        <Header title={title} subtitle="" />
+        <TopMenu />
+        <div><h1>This is the content</h1></div>
+      </body>
     </>
   )
 }
