9799677fefb5e13d937da80734883adf2e577826
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-12-05 22:38:19 +0100

    Add global style. Also tailwind css. Also copyright msg.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+// Copyright 2021 The Authors. Subject to the MIT license.
 import { AppProps } from 'next/app'
+import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
 
 
 export default function App({ Component, pageProps }: AppProps) {
