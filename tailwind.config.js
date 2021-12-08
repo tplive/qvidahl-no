38beeba417410a793672e4df6c9a8eb8758e0501
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-12-08 23:12:40 +0100

    How will this help?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    layers: ['base'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
