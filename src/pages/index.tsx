68267302a4deb5c519ea12f38c178eeee2a65035
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-12-08 20:32:57 +0100

    Add lobsterfont

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,7 @@ const Home: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=UnifrakturCook:wght@700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
       </Head>
 
       <body>
