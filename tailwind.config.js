344100815ceea4410faf98134db33a56e43a83a1
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-12-24 00:28:02 +0100

    Upgrade various packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
 module.exports = {
-  purge: {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}'
-    ],
-    layers: ['utilities', 'components'],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ], 
   theme: {
     extend: {
       fontFamily: {
-        'cards': 'Lobster',
+        cards: 'Lobster',
       },
       padding: {
         '1/2': '50%',
         full: '100%',
-      }
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
