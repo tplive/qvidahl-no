fbffecc11d27c1c5d89e8846b0d652f4382baa25
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2022-03-06 19:13:27 +0100

    Add stuff

---
+++ src/lib/Posts.tsx
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+
+    const files = fs.readdirSync(postsDirectory);
+    const allPostsData = files.map(fileName => {
+        const id = fileName.replace(/\.md$/, '');
+
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+        const matterResult = matter(fileContents);
+
+        return {
+            id,
+            ...matterResult.data
+        }
+    })
+
+    return allPostsData.sort(({ date: a }, { date: b }) => {
+        if (a < b) {
+            return 1;
+        } else if (a > b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    })
+}
